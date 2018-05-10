6baeb0127e2f3f5010bc258982760346fbf987fd
Michael Smith <smithms91@gmail.com>
2018-05-10 07:26:34 -0400

    added start div

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 
 $(document).ready(() => {
-
   $(".card").flip();
+
+  $(".btn_start").on("click", () => {
+    $("#cover").fadeOut();
+    console.log("test");
+  })
+
+let images = ["img/logo.png", "img/lions.png", "img/wings.png", "img/pistons.png", "img/faygo.png", "img/tigers.png"];
+let imagesused = [];
+let rand;
+
+$(".back").each(function() {
+  rand = Math.floor(Math.random() * images.length);
+  $(this).append('<img src="' + images[rand] + '">');
+  if (imagesused.search(images[rand]) != -1) images.splice(rand, 1);
+  else (imagesused.push(images[rand]));
+});
+
+console.log('<img src="' + images[rand] + '">');
+// console.log(images[rand]);
+// console.log(images[rand]);
+
+
+
+
 
 }) 
